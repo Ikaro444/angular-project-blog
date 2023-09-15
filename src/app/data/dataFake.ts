f44b7bc5ab50f363ad16b7dfc7545132d5eadf0c
Ikaro444 <ikaromarkbelem@gmail.com>
2023-09-14 21:29:05 -0400

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photoCover":"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7162622d-6f03-47ff-a8c5-f5df7d3fc632/danicw2-3611d7b0-4acf-45c0-a527-f3ec019b8537.png/v1/fill/w_1280,h_720,q_80,strp/darth_vader_samurai_by_aste17_danicw2-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNzE2MjYyMmQtNmYwMy00N2ZmLWE4YzUtZjVkZjdkM2ZjNjMyXC9kYW5pY3cyLTM2MTFkN2IwLTRhY2YtNDVjMC1hNTI3LWYzZWMwMTliODUzNy5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.feKoCNzI8f8eVYyQjJzVHYINFctxX9i6oUURtD6Ngj4",
+        "title":"saiu o novo samu",
+        "decription": "ola"
+    },
+    {
+        "id":"2",
+        "photoCover":"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7162622d-6f03-47ff-a8c5-f5df7d3fc632/danicw2-3611d7b0-4acf-45c0-a527-f3ec019b8537.png/v1/fill/w_1280,h_720,q_80,strp/darth_vader_samurai_by_aste17_danicw2-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNzE2MjYyMmQtNmYwMy00N2ZmLWE4YzUtZjVkZjdkM2ZjNjMyXC9kYW5pY3cyLTM2MTFkN2IwLTRhY2YtNDVjMC1hNTI3LWYzZWMwMTliODUzNy5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.feKoCNzI8f8eVYyQjJzVHYINFctxX9i6oUURtD6Ngj4",
+        "title":"saiu o novo samu",
+        "decription": "ola"
+    },
+    {
+        "id":"3",
+        "photoCover":"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7162622d-6f03-47ff-a8c5-f5df7d3fc632/danicw2-3611d7b0-4acf-45c0-a527-f3ec019b8537.png/v1/fill/w_1280,h_720,q_80,strp/darth_vader_samurai_by_aste17_danicw2-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNzE2MjYyMmQtNmYwMy00N2ZmLWE4YzUtZjVkZjdkM2ZjNjMyXC9kYW5pY3cyLTM2MTFkN2IwLTRhY2YtNDVjMC1hNTI3LWYzZWMwMTliODUzNy5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.feKoCNzI8f8eVYyQjJzVHYINFctxX9i6oUURtD6Ngj4",
+        "title":"saiu o novo samu",
+        "decription": "ola"
+    },
+    {
+        "id":"4",
+        "photoCover":"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7162622d-6f03-47ff-a8c5-f5df7d3fc632/danicw2-3611d7b0-4acf-45c0-a527-f3ec019b8537.png/v1/fill/w_1280,h_720,q_80,strp/darth_vader_samurai_by_aste17_danicw2-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNzE2MjYyMmQtNmYwMy00N2ZmLWE4YzUtZjVkZjdkM2ZjNjMyXC9kYW5pY3cyLTM2MTFkN2IwLTRhY2YtNDVjMC1hNTI3LWYzZWMwMTliODUzNy5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.feKoCNzI8f8eVYyQjJzVHYINFctxX9i6oUURtD6Ngj4",
+        "title":"saiu o novo samu",
+        "decription": "ola"
+    }
+]
